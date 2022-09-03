104c3419c34691c8a043384e07540d4f3c97de7e
Md Hasan <mdhasan8064@gmail.com>
2022-09-03 09:52:12 +0600

    complate Cetagory news

--- script.js
+++ script.js
@@ -44,27 +44,29 @@ const showProductUi = async (elementId) => {
     productContainer.innerHTML = '';
     // console.log(innerData);
     innerData.forEach(id => {
-        const { title, details, author } = id;
-        const published = author.published_date.slice(0, 10);
+        const { title, details, author, total_view } = id;
+        const published = author.published_date;
+        // console.log(author)
         const div = document.createElement('div');
         div.innerHTML = `
         <div class="card lg:card-side bg-base-100 shadow-xl my-5">
                         <figure><img class='img-sizing' src=${id.image_url} alt="Album"></figure>
                         <div class="card-body">
                             <h2 class="card-title">${title.length > 70 ? title.slice(0, 70) : title}</h2>
-                            <p>${details.length > 120 ? details.slice(0, 120) : details}...</p>
+                            <p class="py-4">${details.length > 120 ? details.slice(0, 120) : details}...</p>
                             <div class="card-actions justify-between items-center">
-                                <div>
-                                    <img src="" alt="">
-                                    <h3>
+                                <div class="flex items-center">
+                                    <div><img src=${author.img} class="h-8 w-8 rounded-full" alt=""></div>
+                                    <div class="pl-2">
+                                    <h3 class="font-medium">
                                     ${author.name}</h3>
-                                    <p>${published}</p>
+                                    <p class="text-sm">${published}</p></div>
                                 </div>
                                 <div>
                                     <i class="fa-regular fa-eye"></i>
-                                    <span>View</span>
+                                    <span>${total_view}</span>
                                 </div>
-                                <button class="btn btn-primary">Listen</button>
+                                <button class="btn btn-primary py-2">Details</button>
                             </div>
                         </div>
                     </div>
